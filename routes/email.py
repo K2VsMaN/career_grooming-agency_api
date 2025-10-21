e33db5b129fb536afb3ce27cafa7f3874c2ac36c
KKB_Official <kkboahene003@gmail.com>
2025-10-21 12:19:50 +0000

    added email endpoint

---
+++ routes/email.py
@@ -0,0 +1,49 @@
+from fastapi import Form, HTTPException, APIRouter
+from pydantic import EmailStr
+from pymongo import MongoClient
+import smtplib
+from email.mime.text import MIMEText
+import os
+from db import career_grooming_db, application_forms_collection, mongo_client
+from dotenv import load_dotenv
+
+load_dotenv()
+
+email_router = APIRouter(tags=["Email"])
+
+EMAIL_USER = os.getenv("EMAIL_USER")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+EMAIL_PORT= int(os.getenv("EMAIL_PORT"))
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+PASSCODE = os.getenv("PASSCODE_REQUIRED")
+
+def send_email(recipient:str, body:str):
+    msg = MIMEText(body, "plain")
+    msg["From"] = EMAIL_USER
+    msg["To"] = recipient
+    msg["Subject"] = body
+
+    with smtplib.SMTP(EMAIL_HOST, EMAIL_PORT) as server:
+        server.starttls()
+        server.login(EMAIL_USER, EMAIL_PASSWORD)
+        server.send_message(msg)
+
+@email_router.post("/send_passcode")
+def send_passcode(email: EmailStr = Form(...)):
+    # Check if user exists
+    form = application_forms_collection.find_one({"trainee_email": email})
+    if not form:
+        raise HTTPException(status_code=404, detail="Email not found in records")
+
+    # Send email with passcode
+    body = f"""
+    Congratulations, you've been selected for our program!
+    Your trainee passcode is: {PASSCODE}
+    Please enter this code to complete your registration.
+    
+    Thank you,
+    Career Grooming Agency Team
+    """
+
+    send_email(email, body)
+    return {"message": f"Passcode sent successfully to {email}"}
